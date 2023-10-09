a0980d6f659e1eddda56cd5d36cab2bccecaca12
hansashi <ahantke@mail.de>
2023-10-09 19:01:32 +0200

    updated screen1

--- components/Screen1.js
+++ components/Screen1.js
@@ -1,9 +1,18 @@
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 const Screen1 = ({ navigation }) => {
+  const [name, setName] = useState('');
+
   return (
     <View style={styles.container}>
       <Text>Hello Screen1!</Text>
+      <TextInput
+        style={styles.textInput}
+        value={name}
+        onChangeText={setName}
+        placeholder='Type your username here'
+      />
       <Button
         title="Go to Screen 2"
         onPress={() => navigation.navigate('Screen2')}
@@ -17,6 +26,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  textInput: {
+    width: "88%",
+    padding: 15,
+    borderWidth: 1,
+    marginTop: 15,
+    marginBottom: 15
   }
 });
 
