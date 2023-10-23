b0813753d101062a68c870e2ff5759963039f3b1
hansashi <ahantke@mail.de>
2023-10-23 19:58:35 +0200

    added chat

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,18 +1,43 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
 
 const ChatScreen = ({ route, navigation }) => {
   const name = route.params.name;
   const bgColor = route.params.bgColor;
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     navigation.setOptions({ title: name });
+
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://picsum.photos/id/64/140/140",
+        },
+      },
+    ]);
   }, []);
 
+  const onSend = (newMessages) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+  }
+
   return (
-    <View style={[styles.container, { backgroundColor: bgColor }]}>
-      <Text>Hello Chatscreen!</Text>
-    </View>
+    // <View style={[styles.container, { backgroundColor: bgColor }]}>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1
+      }}
+    />
+    // </View>
   );
 }
 
